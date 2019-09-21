876728273786758a34220b6b1c45903f1b6b2f07
venkatesh-naik <53571370+venkatesh-naik@users.noreply.github.com>
2019-09-21 14:59:47 +0530

    first commit

---
+++ SimpleGitApp/src/First.java
@@ -0,0 +1,11 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("stated");
+		System.out.println("initiaal stage");
+
+	}
+
+}
